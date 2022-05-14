656b0b1593f6c3297a5e50c8b6a9782b5ed0769a
demelnikov <demelnikov@gmail.com>
2022-05-14 21:26:57 +0300

    First initiate

---
+++ src/main/java/ru/demelnikov/fml/hhanalyser/HhanalyserApplication.java
@@ -0,0 +1,13 @@
+package ru.demelnikov.fml.hhanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhanalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhanalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/demelnikov/fml/hhanalyser/HhanalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.demelnikov.fml.hhanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhanalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
